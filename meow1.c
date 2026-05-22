214dd8c141bf72243df4cc81b3eb907fe75145a8
GringoGrigg <equidza@gmail.com>
2026-05-22 06:20:12 +0300

    arrays added

---
+++ meow1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <cs50.h>
+void meow(int n);
+int main(void)
+{
+    for int i = 0; i < 3; i++)
+    {
+        meow();
+    }
+}
+void meow(int n)
+{
+    printf("meow\n");
+}
