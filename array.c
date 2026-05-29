33640033b15c9062ca97e8ec65bec60f04baf752
GringoGrigg <equidza@gmail.com>
2026-05-29 22:30:05 +0300

    arrays

---
+++ array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+int main(void)
+{
+    int numbers[5] = {1, 2, 3, 4, 5};
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%i\n", numbers[i]);
+    }
+}
