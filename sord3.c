88abb4d9a51075ee601ff4c6f16076e0aa7ee0a1
GringoGrigg <equidza@gmail.com>
2026-06-29 17:12:56 +0300

    meeeeyyy

---
+++ sord3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+void selection_sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int min_index = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_index]) {
+                min_index = j;
+            }
+        }
+        // Swap arr[i] and arr[min_index]
+        int temp = arr[i];
+        arr[i] = arr[min_index];
+        arr[min_index] = temp;
+    }
+}
