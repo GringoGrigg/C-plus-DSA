49f52497d82408e37fa4649279f1ddcf7726649c
GringoGrigg <equidza@gmail.com>
2026-08-26 22:15:23 +0300

    sorted3.c

---
+++ sorted3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+void sort(int values[], int n);
+int main(void)
+{
+    int n = get_int("Number of values: ");
+    int values[n];
+    for (int i = 0; i < n; i++)
+    {
+        values[i] = get_int("Value: ");
+    }
+    sort(values, n);
+    printf("Sorted values:\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%i\n", values[i]);
+    }
+}       
